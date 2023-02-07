2ae75f48c77969ebf7e2fa045e90358563ad1722
guillaumecoin <guillaume.coin@epsi.fr>
2023-02-07 15:21:56 +0100

    Modification

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 
-Requete = requests.get("http://annuairesante.ameli.fr/")
-fichier = BeautifulSoup(Requete.content, "html.parser")
+url = "http://annuairesante.ameli.fr/"
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+}
+
+Requete = requests.Session()
+page = Requete.get(url, headers=header)
+post = Requete.post(url, headers=header)
+#paylod
+
+if page.status_code == 200:
+    lienrecherche = page.url
+
+fichier = BeautifulSoup(page.text, "html.parser")
 with open("page.html", "w") as file:
     file.write(str(fichier))
+
+
