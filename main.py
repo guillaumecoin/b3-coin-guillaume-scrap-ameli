abc03d757481c3a4999ee4b60b7dc78815110642
guillaumecoin <guillaume.coin@epsi.fr>
2023-02-07 14:13:21 +0100

    récupérer la page

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ from bs4 import BeautifulSoup
 
 Requete = requests.get("http://annuairesante.ameli.fr/")
 fichier = BeautifulSoup(Requete.content, "html.parser")
-
 with open("page.html", "w") as file:
     file.write(str(fichier))
